5e2099858705c80b84acd3d59fd515ed8faea441
marek bombera <bomber.marek@gmail.com>
2023-03-07 21:32:10 +0100

    feat: add mock data

---
+++ src/components/heart-rate-graph/mock-data.ts
@@ -0,0 +1,848 @@
+export const mockData = [
+  {
+    timestamp: '2022-07-27T04:28:29.000Z',
+    value: 58,
+  },
+    {
+      timestamp: '2022-07-27T04:28:34.000Z',
+      value: 61,
+    },
+    {
+      timestamp: '2022-07-27T04:28:40.000Z',
+      value: 67,
+    },
+    {
+      timestamp: '2022-07-27T04:28:45.000Z',
+      value: 69,
+    },
+    {
+      timestamp: '2022-07-27T04:28:50.000Z',
+      value: 69,
+    },
+    {
+      timestamp: '2022-07-27T04:28:55.000Z',
+      value: 66,
+    },
+    {
+      timestamp: '2022-07-27T04:29:00.000Z',
+      value: 65,
+    },
+    {
+      timestamp: '2022-07-27T04:29:06.000Z',
+      value: 60,
+    },
+    {
+      timestamp: '2022-07-27T04:29:11.000Z',
+      value: 59,
+    },
+    {
+      timestamp: '2022-07-27T04:29:16.000Z',
+      value: 62,
+    },
+    {
+      timestamp: '2022-07-27T04:29:21.000Z',
+      value: 70,
+    },
+    {
+      timestamp: '2022-07-27T04:29:27.000Z',
+      value: 69,
+    },
+    {
+      timestamp: '2022-07-27T04:29:34.000Z',
+      value: 67,
+    },
+    {
+      timestamp: '2022-07-27T04:29:42.000Z',
+      value: 66,
+    },
+    {
+      timestamp: '2022-07-27T04:29:47.000Z',
+      value: 62,
+    },
+    {
+      timestamp: '2022-07-27T04:29:52.000Z',
+      value: 60,
+    },
+    {
+      timestamp: '2022-07-27T04:29:57.000Z',
+      value: 63,
+    },
+    {
+      timestamp: '2022-07-27T04:30:02.000Z',
+      value: 65,
+    },
+    {
+      timestamp: '2022-07-27T04:30:07.000Z',
+      value: 67,
+    },
+    {
+      timestamp: '2022-07-27T04:30:13.000Z',
+      value: 75,
+    },
+    {
+      timestamp: '2022-07-27T04:30:18.000Z',
+      value: 78,
+    },
+    {
+      timestamp: '2022-07-27T04:30:23.000Z',
+      value: 82,
+    },
+    {
+      timestamp: '2022-07-27T04:30:28.000Z',
+      value: 78,
+    },
+    {
+      timestamp: '2022-07-27T04:30:33.000Z',
+      value: 72,
+    },
+    {
+      timestamp: '2022-07-27T04:30:38.000Z',
+      value: 68,
+    },
+    {
+      timestamp: '2022-07-27T04:30:43.000Z',
+      value: 63,
+    },
+    {
+      timestamp: '2022-07-27T04:30:49.000Z',
+      value: 64,
+    },
+    {
+      timestamp: '2022-07-27T04:30:54.000Z',
+      value: 70,
+    },
+    {
+      timestamp: '2022-07-27T04:30:59.000Z',
+      value: 71,
+    },
+    {
+      timestamp: '2022-07-27T04:31:07.000Z',
+      value: 67,
+    },
+    {
+      timestamp: '2022-07-27T04:31:12.000Z',
+      value: 72,
+    },
+    {
+      timestamp: '2022-07-27T04:31:17.000Z',
+      value: 79,
+    },
+    {
+      timestamp: '2022-07-27T04:31:22.000Z',
+      value: 81,
+    },
+    {
+      timestamp: '2022-07-27T04:31:23.000Z',
+      value: 75,
+    },
+    {
+      timestamp: '2022-07-27T04:31:27.000Z',
+      value: 68,
+    },
+    {
+      timestamp: '2022-07-27T04:31:32.000Z',
+      value: 65,
+    },
+    {
+      timestamp: '2022-07-27T04:31:38.000Z',
+      value: 63,
+    },
+    {
+      timestamp: '2022-07-27T04:31:43.000Z',
+      value: 63,
+    },
+    {
+      timestamp: '2022-07-27T04:31:48.000Z',
+      value: 66,
+    },
+    {
+      timestamp: '2022-07-27T04:31:53.000Z',
+      value: 59,
+    },
+    {
+      timestamp: '2022-07-27T04:31:58.000Z',
+      value: 62,
+    },
+    {
+      timestamp: '2022-07-27T04:32:03.000Z',
+      value: 64,
+    },
+    {
+      timestamp: '2022-07-27T04:32:08.000Z',
+      value: 68,
+    },
+    {
+      timestamp: '2022-07-27T04:32:14.000Z',
+      value: 69,
+    },
+    {
+      timestamp: '2022-07-27T04:32:19.000Z',
+      value: 63,
+    },
+    {
+      timestamp: '2022-07-27T04:32:24.000Z',
+      value: 61,
+    },
+    {
+      timestamp: '2022-07-27T04:32:29.000Z',
+      value: 64,
+    },
+    {
+      timestamp: '2022-07-27T04:32:34.000Z',
+      value: 73,
+    },
+    {
+      timestamp: '2022-07-27T04:32:47.000Z',
+      value: 85,
+    },
+    {
+      timestamp: '2022-07-27T04:32:52.000Z',
+      value: 88,
+    },
+    {
+      timestamp: '2022-07-27T04:32:53.000Z',
+      value: 96,
+    },
+    {
+      timestamp: '2022-07-27T04:32:53.000Z',
+      value: 103,
+    },
+    {
+      timestamp: '2022-07-27T04:32:57.000Z',
+      value: 115,
+    },
+    {
+      timestamp: '2022-07-27T04:33:02.000Z',
+      value: 113,
+    },
+    {
+      timestamp: '2022-07-27T04:33:08.000Z',
+      value: 109,
+    },
+    {
+      timestamp: '2022-07-27T04:33:09.000Z',
+      value: 103,
+    },
+    {
+      timestamp: '2022-07-27T04:33:13.000Z',
+      value: 95,
+    },
+    {
+      timestamp: '2022-07-27T04:33:18.000Z',
+      value: 93,
+    },
+    {
+      timestamp: '2022-07-27T04:33:23.000Z',
+      value: 92,
+    },
+    {
+      timestamp: '2022-07-27T04:33:28.000Z',
+      value: 88,
+    },
+    {
+      timestamp: '2022-07-27T04:33:33.000Z',
+      value: 84,
+    },
+    {
+      timestamp: '2022-07-27T04:33:34.000Z',
+      value: 76,
+    },
+    {
+      timestamp: '2022-07-27T04:35:13.000Z',
+      value: 67,
+    },
+    {
+      timestamp: '2022-07-27T04:35:18.000Z',
+      value: 67,
+    },
+    {
+      timestamp: '2022-07-27T04:35:23.000Z',
+      value: 64,
+    },
+    {
+      timestamp: '2022-07-27T04:35:29.000Z',
+      value: 63,
+    },
+    {
+      timestamp: '2022-07-27T04:35:34.000Z',
+      value: 66,
+    },
+    {
+      timestamp: '2022-07-27T04:35:40.000Z',
+      value: 62,
+    },
+    {
+      timestamp: '2022-07-27T04:35:45.000Z',
+      value: 62,
+    },
+    {
+      timestamp: '2022-07-27T04:35:50.000Z',
+      value: 63,
+    },
+    {
+      timestamp: '2022-07-27T04:35:55.000Z',
+      value: 59,
+    },
+    {
+      timestamp: '2022-07-27T04:36:00.000Z',
+      value: 60,
+    },
+    {
+      timestamp: '2022-07-27T04:36:05.000Z',
+      value: 59,
+    },
+    {
+      timestamp: '2022-07-27T04:36:10.000Z',
+      value: 58,
+    },
+    {
+      timestamp: '2022-07-27T04:36:15.000Z',
+      value: 58,
+    },
+    {
+      timestamp: '2022-07-27T04:36:21.000Z',
+      value: 60,
+    },
+    {
+      timestamp: '2022-07-27T04:36:26.000Z',
+      value: 61,
+    },
+    {
+      timestamp: '2022-07-27T04:36:31.000Z',
+      value: 60,
+    },
+    {
+      timestamp: '2022-07-27T04:36:36.000Z',
+      value: 61,
+    },
+    {
+      timestamp: '2022-07-27T04:36:41.000Z',
+      value: 61,
+    },
+    {
+      timestamp: '2022-07-27T04:36:46.000Z',
+      value: 63,
+    },
+    {
+      timestamp: '2022-07-27T04:36:52.000Z',
+      value: 62,
+    },
+    {
+      timestamp: '2022-07-27T04:36:57.000Z',
+      value: 62,
+    },
+    {
+      timestamp: '2022-07-27T04:37:02.000Z',
+      value: 62,
+    },
+    {
+      timestamp: '2022-07-27T04:37:07.000Z',
+      value: 64,
+    },
+    {
+      timestamp: '2022-07-27T04:37:08.000Z',
+      value: 68,
+    },
+    {
+      timestamp: '2022-07-27T04:37:12.000Z',
+      value: 76,
+    },
+    {
+      timestamp: '2022-07-27T04:37:14.000Z',
+      value: 82,
+    },
+    {
+      timestamp: '2022-07-27T04:37:17.000Z',
+      value: 88,
+    },
+    {
+      timestamp: '2022-07-27T04:37:22.000Z',
+      value: 93,
+    },
+    {
+      timestamp: '2022-07-27T04:37:28.000Z',
+      value: 88,
+    },
+    {
+      timestamp: '2022-07-27T04:37:33.000Z',
+      value: 85,
+    },
+    {
+      timestamp: '2022-07-27T04:37:38.000Z',
+      value: 79,
+    },
+    {
+      timestamp: '2022-07-27T04:37:43.000Z',
+      value: 86,
+    },
+    {
+      timestamp: '2022-07-27T04:37:48.000Z',
+      value: 86,
+    },
+    {
+      timestamp: '2022-07-27T04:37:53.000Z',
+      value: 78,
+    },
+    {
+      timestamp: '2022-07-27T04:37:58.000Z',
+      value: 76,
+    },
+    {
+      timestamp: '2022-07-27T04:38:06.000Z',
+      value: 71,
+    },
+    {
+      timestamp: '2022-07-27T04:38:11.000Z',
+      value: 71,
+    },
+    {
+      timestamp: '2022-07-27T04:38:16.000Z',
+      value: 65,
+    },
+    {
+      timestamp: '2022-07-27T04:38:22.000Z',
+      value: 63,
+    },
+    {
+      timestamp: '2022-07-27T04:38:27.000Z',
+      value: 62,
+    },
+    {
+      timestamp: '2022-07-27T04:38:32.000Z',
+      value: 61,
+    },
+    {
+      timestamp: '2022-07-27T04:38:37.000Z',
+      value: 60,
+    },
+    {
+      timestamp: '2022-07-27T04:38:42.000Z',
+      value: 58,
+    },
+    {
+      timestamp: '2022-07-27T04:38:47.000Z',
+      value: 57,
+    },
+    {
+      timestamp: '2022-07-27T04:38:53.000Z',
+      value: 58,
+    },
+    {
+      timestamp: '2022-07-27T04:38:58.000Z',
+      value: 58,
+    },
+    {
+      timestamp: '2022-07-27T04:39:03.000Z',
+      value: 59,
+    },
+    {
+      timestamp: '2022-07-27T04:39:08.000Z',
+      value: 59,
+    },
+    {
+      timestamp: '2022-07-27T04:39:13.000Z',
+      value: 65,
+    },
+    {
+      timestamp: '2022-07-27T04:39:18.000Z',
+      value: 65,
+    },
+    {
+      timestamp: '2022-07-27T04:39:23.000Z',
+      value: 67,
+    },
+    {
+      timestamp: '2022-07-27T04:39:28.000Z',
+      value: 65,
+    },
+    {
+      timestamp: '2022-07-27T04:39:36.000Z',
+      value: 68,
+    },
+    {
+      timestamp: '2022-07-27T04:39:41.000Z',
+      value: 78,
+    },
+    {
+      timestamp: '2022-07-27T04:39:47.000Z',
+      value: 78,
+    },
+    {
+      timestamp: '2022-07-27T04:39:52.000Z',
+      value: 95,
+    },
+    {
+      timestamp: '2022-07-27T04:39:57.000Z',
+      value: 97,
+    },
+    {
+      timestamp: '2022-07-27T04:40:02.000Z',
+      value: 89,
+    },
+    {
+      timestamp: '2022-07-27T04:40:07.000Z',
+      value: 77,
+    },
+    {
+      timestamp: '2022-07-27T04:40:12.000Z',
+      value: 69,
+    },
+    {
+      timestamp: '2022-07-27T04:40:17.000Z',
+      value: 69,
+    },
+    {
+      timestamp: '2022-07-27T04:40:23.000Z',
+      value: 69,
+    },
+    {
+      timestamp: '2022-07-27T04:40:28.000Z',
+      value: 65,
+    },
+    {
+      timestamp: '2022-07-27T04:40:33.000Z',
+      value: 64,
+    },
+    {
+      timestamp: '2022-07-27T04:32:03.000Z',
+      value: 60,
+    },
+    {
+      timestamp: '2022-07-27T04:32:08.000Z',
+      value: 63,
+    },
+    {
+      timestamp: '2022-07-27T04:32:13.000Z',
+      value: 64,
+    },
+    {
+      timestamp: '2022-07-27T04:32:19.000Z',
+      value: 63,
+    },
+    {
+      timestamp: '2022-07-27T04:32:24.000Z',
+      value: 63,
+    },
+    {
+      timestamp: '2022-07-27T04:32:29.000Z',
+      value: 63,
+    },
+    {
+      timestamp: '2022-07-27T04:32:34.000Z',
+      value: 64,
+    },
+    {
+      timestamp: '2022-07-27T04:32:39.000Z',
+      value: 66,
+    },
+    {
+      timestamp: '2022-07-27T04:32:45.000Z',
+      value: 68,
+    },
+    {
+      timestamp: '2022-07-27T04:32:50.000Z',
+      value: 72,
+    },
+    {
+      timestamp: '2022-07-27T04:32:55.000Z',
+      value: 73,
+    },
+    {
+      timestamp: '2022-07-27T04:33:00.000Z',
+      value: 72,
+    },
+    {
+      timestamp: '2022-07-27T04:33:06.000Z',
+      value: 70,
+    },
+    {
+      timestamp: '2022-07-27T04:33:11.000Z',
+      value: 67,
+    },
+    {
+      timestamp: '2022-07-27T04:33:16.000Z',
+      value: 63,
+    },
+    {
+      timestamp: '2022-07-27T04:33:21.000Z',
+      value: 61,
+    },
+    {
+      timestamp: '2022-07-27T04:33:26.000Z',
+      value: 60,
+    },
+    {
+      timestamp: '2022-07-27T04:33:31.000Z',
+      value: 62,
+    },
+    {
+      timestamp: '2022-07-27T04:33:37.000Z',
+      value: 64,
+    },
+    {
+      timestamp: '2022-07-27T04:33:42.000Z',
+      value: 65,
+    },
+    {
+      timestamp: '2022-07-27T04:33:47.000Z',
+      value: 65,
+    },
+    {
+      timestamp: '2022-07-27T04:33:52.000Z',
+      value: 65,
+    },
+    {
+      timestamp: '2022-07-27T04:33:57.000Z',
+      value: 66,
+    },
+    {
+      timestamp: '2022-07-27T04:34:02.000Z',
+      value: 65,
+    },
+    {
+      timestamp: '2022-07-27T04:34:08.000Z',
+      value: 64,
+    },
+    {
+      timestamp: '2022-07-27T04:34:13.000Z',
+      value: 63,
+    },
+    {
+      timestamp: '2022-07-28T04:32:39.000Z',
+      value: 67,
+    },
+    {
+      timestamp: '2022-07-28T04:32:44.000Z',
+      value: 67,
+    },
+    {
+      timestamp: '2022-07-28T04:32:49.000Z',
+      value: 67,
+    },
+    {
+      timestamp: '2022-07-28T04:32:55.000Z',
+      value: 68,
+    },
+    {
+      timestamp: '2022-07-28T04:33:00.000Z',
+      value: 69,
+    },
+    {
+      timestamp: '2022-07-28T04:33:05.000Z',
+      value: 70,
+    },
+    {
+      timestamp: '2022-07-28T04:33:10.000Z',
+      value: 70,
+    },
+    {
+      timestamp: '2022-07-28T04:33:15.000Z',
+      value: 69,
+    },
+    {
+      timestamp: '2022-07-28T04:33:20.000Z',
+      value: 68,
+    },
+    {
+      timestamp: '2022-07-28T04:33:26.000Z',
+      value: 67,
+    },
+    {
+      timestamp: '2022-07-28T04:33:31.000Z',
+      value: 68,
+    },
+    {
+      timestamp: '2022-07-28T04:33:36.000Z',
+      value: 69,
+    },
+    {
+      timestamp: '2022-07-28T04:33:41.000Z',
+      value: 70,
+    },
+    {
+      timestamp: '2022-07-28T04:33:46.000Z',
+      value: 71,
+    },
+    {
+      timestamp: '2022-07-28T04:33:52.000Z',
+      value: 70,
+    },
+    {
+      timestamp: '2022-07-28T04:33:57.000Z',
+      value: 68,
+    },
+  ]
+
+export type MockData = typeof mockData
+  
+
+const fiftyMoreMockData = [
+    {
+        timestamp: '2022-07-27T04:32:03.000Z',
+        value: 60,
+      },
+      {
+        timestamp: '2022-07-27T04:32:08.000Z',
+        value: 63,
+      },
+      {
+        timestamp: '2022-07-27T04:32:13.000Z',
+        value: 64,
+      },
+      {
+        timestamp: '2022-07-27T04:32:19.000Z',
+        value: 63,
+      },
+      {
+        timestamp: '2022-07-27T04:32:24.000Z',
+        value: 63,
+      },
+      {
+        timestamp: '2022-07-27T04:32:29.000Z',
+        value: 63,
+      },
+      {
+        timestamp: '2022-07-27T04:32:34.000Z',
+        value: 64,
+      },
+      {
+        timestamp: '2022-07-27T04:32:39.000Z',
+        value: 66,
+      },
+      {
+        timestamp: '2022-07-27T04:32:45.000Z',
+        value: 68,
+      },
+      {
+        timestamp: '2022-07-27T04:32:50.000Z',
+        value: 72,
+      },
+      {
+        timestamp: '2022-07-27T04:32:55.000Z',
+        value: 73,
+      },
+      {
+        timestamp: '2022-07-27T04:33:00.000Z',
+        value: 72,
+      },
+      {
+        timestamp: '2022-07-27T04:33:06.000Z',
+        value: 70,
+      },
+      {
+        timestamp: '2022-07-27T04:33:11.000Z',
+        value: 67,
+      },
+      {
+        timestamp: '2022-07-27T04:33:16.000Z',
+        value: 63,
+      },
+      {
+        timestamp: '2022-07-27T04:33:21.000Z',
+        value: 61,
+      },
+      {
+        timestamp: '2022-07-27T04:33:26.000Z',
+        value: 60,
+      },
+      {
+        timestamp: '2022-07-27T04:33:31.000Z',
+        value: 62,
+      },
+      {
+        timestamp: '2022-07-27T04:33:37.000Z',
+        value: 64,
+      },
+      {
+        timestamp: '2022-07-27T04:33:42.000Z',
+        value: 65,
+      },
+      {
+        timestamp: '2022-07-27T04:33:47.000Z',
+        value: 65,
+      },
+      {
+        timestamp: '2022-07-27T04:33:52.000Z',
+        value: 65,
+      },
+      {
+        timestamp: '2022-07-27T04:33:57.000Z',
+        value: 66,
+      },
+      {
+        timestamp: '2022-07-27T04:34:02.000Z',
+        value: 65,
+      },
+      {
+        timestamp: '2022-07-27T04:34:08.000Z',
+        value: 64,
+      },
+      {
+        timestamp: '2022-07-27T04:34:13.000Z',
+        value: 63,
+      },
+      {
+        timestamp: '2022-07-28T04:32:39.000Z',
+        value: 67,
+      },
+      {
+        timestamp: '2022-07-28T04:32:44.000Z',
+        value: 67,
+      },
+      {
+        timestamp: '2022-07-28T04:32:49.000Z',
+        value: 67,
+      },
+      {
+        timestamp: '2022-07-28T04:32:55.000Z',
+        value: 68,
+      },
+      {
+        timestamp: '2022-07-28T04:33:00.000Z',
+        value: 69,
+      },
+      {
+        timestamp: '2022-07-28T04:33:05.000Z',
+        value: 70,
+      },
+      {
+        timestamp: '2022-07-28T04:33:10.000Z',
+        value: 70,
+      },
+      {
+        timestamp: '2022-07-28T04:33:15.000Z',
+        value: 69,
+      },
+      {
+        timestamp: '2022-07-28T04:33:20.000Z',
+        value: 68,
+      },
+      {
+        timestamp: '2022-07-28T04:33:26.000Z',
+        value: 67,
+      },
+      {
+        timestamp: '2022-07-28T04:33:31.000Z',
+        value: 68,
+      },
+      {
+        timestamp: '2022-07-28T04:33:36.000Z',
+        value: 69,
+      },
+      {
+        timestamp: '2022-07-28T04:33:41.000Z',
+        value: 70,
+      },
+      {
+        timestamp: '2022-07-28T04:33:46.000Z',
+        value: 71,
+      },
+      {
+        timestamp: '2022-07-28T04:33:52.000Z',
+        value: 70,
+      },
+      {
+        timestamp: '2022-07-28T04:33:57.000Z',
+        value: 68,
+      },
+]
